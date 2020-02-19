63dd5078c276182aa862bed964d8542a9f751949
Gavrilov Kirill <gavrilovskirill@gmail.com>
2020-02-19 23:26:34 +0700

    Fixed variables

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 const createError = require('http-errors');
 const indexRouter = require('./src/routes/index');
+var cors = require('cors')
 require('dotenv').config()
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 7878;
+app.use(cors());
 app.use(express.json());
 app.use('/', indexRouter);
 // catch 404 and forward to error handler
--- src/models/mails/index.js
+++ src/models/mails/index.js
@@ -1,29 +1,31 @@
 const nodemailer = require('nodemailer');    
 
-const USER = process.env.MAIL;
-const PASS = process.env.PASSWORD;
-const MAIL_TO = process.env.MAIL_TO;
-
 module.exports = {
     async sendMail(params){
+        console.log("params", params)
         // return {response: params}
+        var html_mes = "Пришла заявка с сайта \n ";
+        html_mes += params.name ? "Имя: " + params.name + '\n' : '';
+        html_mes += params.phone ? "Телефон: " + params.phone + '\n' : '';
+        html_mes += params.email ? "Email: " + params.email + '\n' : '';
+        html_mes += params.theme ? "Тема: " + params.theme + '\n' : '';
+        html_mes += params.msg ? "Сообщение: " + params.msg + '\n' : '';
         try{
             const transporter = nodemailer.createTransport({
-                service: 'gmail',
+                service: 'yandex',
                 auth: {
                     user: USER,
                     pass: PASS
                 }
             });
-
             const mail_to = MAIL_TO;
-            
             const result = await transporter.sendMail({
-                from: '"Node js" <linkidin1000@gmail.com>',
+                from: '" Сообщение с сайта" <admin@vulcanicawater.com>',
                 to: mail_to,
-                subject: "Message from Node js",
-                text: JSON.stringify(params)
+                subject: "Message from site vulcanicawater.com",
+                text: html_mes
             });
+            console.log("Отправил: ", html_mes);
             return result;
         } catch(e) {
             return { response:'Error', error: e }
